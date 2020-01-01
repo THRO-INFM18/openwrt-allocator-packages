9cc9e7f7aa852d8b6383101e74e1b22719cc7f65
Johann Neuhauser <johann@it-neuhauser.de>
2020-01-01 21:49:29 +0100

    malloc-benchmark: make script more compliant to POSIX sh

--- bench/malloc-benchmarks/files/run-malloc-benchmarks.sh
+++ bench/malloc-benchmarks/files/run-malloc-benchmarks.sh
@@ -23,16 +23,17 @@ xmalloc-test
 LD_PATHS="/lib /usr/lib /usr/local/lib"
 
 # auto cfg
-NPROC=$(cat /proc/cpuinfo | grep processor | wc -l)
+NPROC=$( grep -c processor /proc/cpuinfo )
 
 echo "Searching for available malloc libraries..."
 LD_PATHS_TMP=
 for d in $LD_PATHS ; do
-    [ -d $d ] && LD_PATHS_TMP="$LD_PATHS_TMP $d"
+    [ -d "$d" ] && LD_PATHS_TMP="$LD_PATHS_TMP $d"
 done
 LD_PATHS=$LD_PATHS_TMP
 AVAILABLE_MALLOC_LIBS=""
 for name in $MALLOC_LIBS ; do
+    # We need word splitting for LD_PATHS, so no double quotes here!
     so=$( find $LD_PATHS -type f -iname "${name}*" | sort | tail -n 1 )
     if [ -f "$so" ] ; then
         echo " Found: $so"
@@ -43,7 +44,7 @@ done
 echo "Searching for available benchmarks..."
 AVAILABLE_BENCHMARKS=""
 for bench in $ENABLED_TESTS ; do
-    if command -v $bench >/dev/null 2>&1 ; then
+    if command -v "$bench" >/dev/null 2>&1 ; then
         echo " Found: $bench"
         AVAILABLE_BENCHMARKS="$AVAILABLE_BENCHMARKS $bench"
     fi
@@ -63,62 +64,62 @@ run_with_time() {
     [ $# -gt 0 ] && args=$@
     local lib=${LD_PRELOAD##*/}
     lib=${lib%%.so*}
-    GNU_TIME="$(which time) --quiet --append --output $RESULT_FILE"
-    $GNU_TIME --format "%U\t%S\t%E\t%X\t%D\t%M\t%F\t%R\t%W\t$lib\t$name\t$cmd\t$args" $cmd $args < $input > $output 2>&1
+    GNU_TIME="$( type -P time ) --quiet --append --output $RESULT_FILE"
+    echo "Running $name with $lib"
+    # We need word splitting for args, so no double quotes here!
+    $GNU_TIME --format "%U\\t%S\\t%E\\t%X\\t%D\\t%M\\t%F\\t%R\\t%W\\t$lib\\t$name\\t$cmd\\t$args" "$cmd" $args < "$input" > "$output" 2>&1
 }
 
 run_benchmark() {
     local bench=$1
-    local infile=/dev/null
 
-    echo "Running $bench"
     case "$bench" in
         alloc-test|\
         sh6bench|\
         sh8bench)
-            run_with_time /dev/null /dev/null $bench $bench 1
-            if [ $NPROC -gt 1 ] ; then
-                run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC
+            run_with_time /dev/null /dev/null "$bench" "$bench" 1
+            if [ "$NPROC" -gt 1 ] ; then
+                run_with_time /dev/null /dev/null "$bench$NPROC" "$bench" "$NPROC"
             fi
             ;;
         barnes)
-            run_with_time /usr/share/barnes/input /dev/null $bench $bench
+            run_with_time /usr/share/barnes/input /dev/null "$bench" "$bench"
             ;;
         cache-scratch|\
         cache-thrash)
-            run_with_time /dev/null /dev/null $bench $bench 1 1000 1 2000000 1
-            if [ $NPROC -gt 1 ] ; then
-                run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC 1000 1 2000000 $NPROC
+            run_with_time /dev/null /dev/null "$bench" "$bench" 1 1000 1 2000000 1
+            if [ "$NPROC" -gt 1 ] ; then
+                run_with_time /dev/null /dev/null "$bench$NPROC" "$bench" "$NPROC" 1000 1 2000000 "$NPROC"
             fi
             ;;
         cfrac)
-            run_with_time /dev/null /dev/null $bench $bench 175451865205073170563711388363274837927895
+            run_with_time /dev/null /dev/null "$bench" "$bench" 175451865205073170563711388363274837927895
             ;;
         espresso)
-            run_with_time /dev/null /dev/null $bench $bench -s /usr/share/espresso/largest.espresso
+            run_with_time /dev/null /dev/null "$bench" "$bench" -s /usr/share/espresso/largest.espresso
             ;;
         larson)
-            run_with_time /dev/null /dev/null $bench $bench 2.5 7 8 1000 10000 42 100
+            run_with_time /dev/null /dev/null "$bench" "$bench" 2.5 7 8 1000 10000 42 100
             ;;
         malloc-large)
-            run_with_time /dev/null /dev/null $bench $bench
+            run_with_time /dev/null /dev/null "$bench" "$bench"
             ;;
         mstress)
-            run_with_time /dev/null /dev/null $bench $bench 1 200
-            if [ $NPROC -gt 1 ] ; then
-                run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC 200
+            run_with_time /dev/null /dev/null "$bench" "$bench" 1 200
+            if [ "$NPROC" -gt 1 ] ; then
+                run_with_time /dev/null /dev/null "$bench$NPROC" "$bench" "$NPROC" 200
             fi
             ;;
         rptest)
-            run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC 0 2 2 500 1000 200 8 64000
+            run_with_time /dev/null /dev/null "$bench$NPROC" "$bench" "$NPROC" 0 2 2 500 1000 200 8 64000
             #run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC 0 1 2 1000 1000 500 8 64000
             #run_with_time /dev/null /dev/null $bench$NPROC $bench $NPROC 0 2 2 500 1000 200 16 1600000
-            let threads=4*$NPROC
-            run_with_time /dev/null /dev/null $bench$threads $bench $threads 0 2 2 500 1000 200 8 64000
+            threads=$((4*NPROC))
+            run_with_time /dev/null /dev/null "$bench$threads" "$bench" "$threads" 0 2 2 500 1000 200 8 64000
             ;;
         xmalloc-test)
-            let threads=4*$NPROC
-            run_with_time /dev/null /dev/null $bench $bench -w $threads -t 5 -s '-1'
+            threads=$((4*NPROC))
+            run_with_time /dev/null /dev/null "$bench" "$bench" -w $threads -t 5 -s '-1'
             ;;
         *)
             echo "ERROR: unknown benchmark: $bench"
@@ -131,7 +132,7 @@ run_available_benchmarks_with_available_malloc_libs() {
         LD_PRELOAD="/lib/libatomic.so.1 $lib"
         export LD_PRELOAD
         for bench in $AVAILABLE_BENCHMARKS ; do
-            run_benchmark $bench
+            run_benchmark "$bench"
         done
     done
 }
@@ -139,11 +140,11 @@ run_available_benchmarks_with_available_malloc_libs() {
 create_csv() {
     SEP=";"
     [ $# -gt 0 ] && SEP=$1
-    sed "s/[[:space:]]/$SEP/g" $RESULT_FILE > ${RESULT_FILE}.csv
+    sed "s/[[:space:]]/$SEP/g" "$RESULT_FILE" > "${RESULT_FILE}.csv"
 }
 
-mkdir -p $(dirname $RESULT_FILE)
+mkdir -p "$(dirname "$RESULT_FILE")"
 
 run_available_benchmarks_with_available_malloc_libs
 
-create_csv
+create_csv ","
